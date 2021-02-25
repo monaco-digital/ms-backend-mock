54b88c5b50d5b5e11fe72b2abc3100c3886f3f4e
Paul Grimmer <paul.grimmer@groupimd.com>
2021-02-25 09:00:12 +0000

    Adding cors headers

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const topics = require('./data/topics.json')
-const paragraphs = require('./data/paragraphs.json')
+const topics = require("./data/topics.json");
+const paragraphs = require("./data/paragraphs.json");
+var cors = require("cors");
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/paragraphs', (req, res) => {
-  res.json(paragraphs)
-})
+app.use(cors());
 
-app.get('/topics', (req, res) => {
-  res.json(topics)
-})
+app.get("/paragraphs", (req, res) => {
+  res.json(paragraphs);
+});
+
+app.get("/topics", (req, res) => {
+  res.json(topics);
+});
 
 app.listen(port, () => {
-  console.log(`Ms backend mock listening at http://localhost:${port}`)
-})
+  console.log(`Ms backend mock listening at http://localhost:${port}`);
+});
